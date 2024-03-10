0a1c70a7b9b63d67f78864472607ff04e2cdab48
Yash Singh <saiansh2525@gmail.com>
2024-03-10 10:52:21 -0700

    chore: move deps to parent workspace

--- dialog/src/components/CodemirrorEditor.vue
+++ dialog/src/components/CodemirrorEditor.vue
@@ -115,7 +115,7 @@ onMounted(() => {
                 callback: (updatedView) => {
                   const state = updatedView.state.field(breakpointState);
                   breakpoints.value = [];
-                  // @ts-expect-error -- RangeSet.length does seem to exist on the console, types not up to date
+                  // @ts-expect-error -- RangeSet.length is an internal field, but we need it
                   state.between(0, state.length, (from) => {
                     breakpoints.value.push(
                       updatedView.state.doc.lineAt(from).number
